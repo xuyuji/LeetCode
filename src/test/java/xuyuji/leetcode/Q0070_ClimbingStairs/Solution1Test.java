ce84d1d06a376f3e39c987301a67a147aedd2db3
xuyuji <xuyuji1987@163.com>
2018-08-01 10:59:46 +0800

    rename

--- src/test/java/xuyuji/leetcode/Q0070_ClimbingStairs/Solution1Test.java
+++ src/test/java/xuyuji/leetcode/Q0070_ClimbingStairs/Solution1Test.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Before;
 import org.junit.Test;
 
-public class SolutionTest {
+public class Solution1Test {
 
 	private Solution1 solution;
 
