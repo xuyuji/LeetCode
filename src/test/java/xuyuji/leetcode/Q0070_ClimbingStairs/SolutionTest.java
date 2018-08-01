5cb94509ccbec945855870d176c96e140fddf913
xuyuji <xuyuji1987@163.com>
2018-08-01 10:51:07 +0800

    add 70.Climbing Stairs

---
+++ src/test/java/xuyuji/leetcode/Q0070_ClimbingStairs/SolutionTest.java
@@ -0,0 +1,31 @@
+package xuyuji.leetcode.Q0070_ClimbingStairs;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class SolutionTest {
+
+	private Solution1 solution;
+
+	@Before
+	public void setup() {
+		solution = new Solution1();
+	}
+
+	@Test
+	public void testClimbStairs1() {
+		assertEquals(2, solution.climbStairs(2));
+	}
+
+	@Test
+	public void testClimbStairs2() {
+		assertEquals(3, solution.climbStairs(3));
+	}
+
+	@Test
+	public void testClimbStairs3() {
+		assertEquals(1134903170, solution.climbStairs(44));
+	}
+}
